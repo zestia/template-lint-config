d73d4b700e66a453fb9769d2b6d6163dfaa8dda9
Andrew Kirwin <andrew.kirwin@gmail.com>
2018-01-16 17:46:31 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# @zestia/ember-template-lint-plugin
+
+```javascript
+// .template-lintrc.js
+module.exports = {
+  plugins: ['@zestia/ember-template-lint-plugin'],
+  extends: 'zestia:recommended'
+};
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+  name: 'zestia',
+
+  rules: {
+
+  },
+
+  configurations: {
+    recommended: {
+      extends: 'recommended',
+      rules: {
+        'attribute-indentation': true,
+        'bare-strings': false,
+        'block-indentation': true,
+        'deprecated-each-syntax': true,
+        'deprecated-inline-view-helper': true,
+        'deprecated-render-helper': true,
+        'eol-last': 'never',
+        'html-comments': true,
+        'img-alt-attributes': false,
+        'inline-link-to': false,
+        'inline-styles': true,
+        'invalid-interactive': true,
+        'link-rel-noopener': 'strict',
+        'nested-interactive': true,
+        'no-debugger': true,
+        'no-duplicate-attributes': true,
+        'no-log': true,
+        'no-trailing-spaces': true,
+        'quotes': 'double',
+        'self-closing-void-elements': true,
+        'simple-unless': true,
+        'style-concatenation': true,
+        'table-groups': false,
+        'template-length': false,
+        'triple-curlies': true,
+        'unused-block-params': true
+      }
+    }
+  }
+};
