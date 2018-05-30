4e104046fcdd984c698ed2163d3bba0909d48b3e
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-05-30 10:25:05 +0100

    remove deprecated rule names

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ module.exports = {
       extends: 'recommended',
       rules: {
         // 'attribute-indentation': true,
-        'bare-strings': false,
         'block-indentation': true,
         'deprecated-inline-view-helper': true,
         'deprecated-render-helper': true,
         'eol-last': 'never',
-        'html-comments': true,
         'img-alt-attributes': false,
         'inline-link-to': false,
-        'inline-styles': true,
-        'invalid-interactive': true,
         'link-rel-noopener': 'strict',
-        'nested-interactive': true,
+        'no-bare-strings': false,
         'no-debugger': true,
         'no-duplicate-attributes': true,
+        'no-html-comments': true,
+        'no-inline-styles': true,
+        'no-invalid-interactive': true,
         'no-log': true,
+        'no-nested-interactive': true,
         'no-trailing-spaces': true,
+        'no-triple-curlies': true,
+        'no-unused-block-params': true
         'quotes': 'double',
         'self-closing-void-elements': true,
         'simple-unless': true,
         'style-concatenation': true,
         'table-groups': false,
         'template-length': false,
-        'triple-curlies': true,
-        'unused-block-params': true
       }
     }
   }
