a389451c382d259fa66a61185f160312a6ea796a
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-08-07 10:01:10 +0100

    disable rule temporarily

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ module.exports = {
         'link-rel-noopener': 'strict',
         'no-bare-strings': false,
         'invocable-blacklist': [],
+        'no-implicit-this': false,
         // 'no-implicit-this': true,
         'no-debugger': true,
         'no-duplicate-attributes': true,
