44343c2868453a63b4e17cdd76ccff09b628c45e
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-06-14 12:05:09 +0100

    add new rules that are set to off in the recommended config

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ module.exports = {
       extends: 'recommended',
       rules: {
         'attribute-indentation': false,
+        'eol-last': 'never',
         'img-alt-attributes': false,
+        'link-href-attributes': true,
         'link-rel-noopener': 'strict',
+        'no-abstract-roles': true,
+        'no-extra-mut-helper-argument': true,
         'no-implicit-this': true,
         'no-negated-condition': true,
-        'no-extra-mut-helper-argument': true
+        'no-positive-tabindex': true,
+        'no-trailing-spaces': true
       }
     }
   }
 };
+
