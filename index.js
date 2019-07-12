701b59bfc089a2abacd06fda4c56783af0a54b5f
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-07-12 16:32:09 +0100

    prepare for new rule

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 module.exports = {
   name: 'zestia',
 
+  /**
+   * Commented out rules are because of:
+   * https://github.com/ember-template-lint/ember-template-lint/issues/666
+   */
+
   configurations: {
     recommended: {
       extends: 'recommended',
       rules: {
         'attribute-indentation': false,
         'eol-last': 'never',
-        'img-alt-attributes': false,
         'link-href-attributes': true,
         'link-rel-noopener': 'strict',
         // 'no-abstract-roles': true,
@@ -15,7 +19,9 @@ module.exports = {
         'no-implicit-this': true,
         // 'no-negated-condition': true,
         // 'no-positive-tabindex': true,
-        'no-trailing-spaces': true
+        'no-trailing-spaces': true,
+        'img-alt-attributes': false,
+        // 'require-valid-alt-text': false
       }
     }
   }
